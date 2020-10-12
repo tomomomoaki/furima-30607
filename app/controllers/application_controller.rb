2e942827c40df93b196e73b38760096faa8f8f7d
tomomomoaki <tomomomomorou333@gmail.com>
2020-10-12 18:22:14 +0900

    ストロングパラメーターの記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters,if: :devise_controller?
+
+  private
+
+  def configure_parmitted_parameters
+    devise_parameters_sanitizer.permit(sign_up, keys:[:nickname, :last_name, :first_name, :last_name_key, :first_name_key, :birthday])
+  end
 end
