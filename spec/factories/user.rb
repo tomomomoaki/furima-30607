6fd08ea5387a51064049965001527ac1750b7b20
tomomomoaki <tomomomomorou333@gmail.com>
2020-10-13 12:23:19 +0900

    factorybotで模擬ユーザーの設定

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname
-    email
+    nickname                {Faker::Name.initials(number: 3)}
+    email                   {Faker::Internet.free_email}
+    password                {Faker::Internet.password(min_length: 6)}
+    password_confirmation   {password}
+    last_name               {Faker::Japanese::Name.last_name}
+    first_name              {Faker::Japanese::Name.first_name}
+    last_name_key           {"アアア"}
+    first_name_key          {"イイイ"}
+    birthday                {Faker::Date.backword}
   end
 end
