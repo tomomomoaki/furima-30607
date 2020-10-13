29102dd78129952c9e7fe9a5fb992667b92bae96
tomomomoaki <tomomomomorou333@gmail.com>
2020-10-13 17:15:47 +0900

    userモデルの単体テストコードの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,16 @@ class User < ApplicationRecord
   validates :nickname, presence: :true
   validates :birthday, presence: :true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  validates_format_of :email, with: /@/ , message: 'には「＠」を含めて設定してください' 
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'には全角文字を使用してください' } do
     validates :last_name
     validates :first_name
   end
-    with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'には全角カタカナを使用してください' } do
     validates :last_name_key
     validates :first_name_key
   end
