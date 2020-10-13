41bba6cbd8e4664b237b5409cd4be4d0e8a075b7
tomomomoaki <tomomomomorou333@gmail.com>
2020-10-13 11:08:26 +0900

    全角バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
   with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
     validates :last_name
     validates :first_name
+  end
+    with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
     validates :last_name_key
     validates :first_name_key
   end
