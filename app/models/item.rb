0ad5b0cc455c451a57e759105be69d45ce6b94a2
tomomomoaki <tomomomomorou333@gmail.com>
2020-10-14 14:46:22 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :day_to_ship
+
+  belongs_to :user
 end
