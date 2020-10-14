497979f61fbe26712ab36cb69fa8651ba5bf31b0
tomomomoaki <tomomomomorou333@gmail.com>
2020-10-14 14:57:20 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
 
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :explanation, presence: true
+  validates :category_id, :status_id, :prefecture_id, :shipping_charge_id, :day_to_ship_id, presence: true
+  validates :category_id, :status_id, :prefecture_id, :shipping_charge_id, :day_to_ship_id, numericality:{other_than:1}
+
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
