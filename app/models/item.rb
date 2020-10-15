a9fc456e313e2dd569a2bcbc0efc69c3cdb4b2a8
tomomomoaki <tomomomomorou333@gmail.com>
2020-10-15 11:12:17 +0900

    priceバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
   validates :name, presence: true
+  
   validates :price, presence: true
+  validates :price, numericality: {only_interger: true, message: 'は数字のみで記入してください' }
+  validates  :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:'は300~9,999,999円で設定してください'}
+  
+
   validates :explanation, presence: true
   validates :category_id, :status_id, :prefecture_id, :shipping_charge_id, :days_to_ship_id, presence: true
   validates :category_id, :status_id, :prefecture_id, :shipping_charge_id, :days_to_ship_id, numericality: { other_than: 1 }
