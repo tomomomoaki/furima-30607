e129af6dafec27089935be8797a3375dbec6dfe0
tomomomoaki <tomomomomorou333@gmail.com>
2020-10-16 10:29:19 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :redirect_signin, only: :new
+  before_action :authenticate_user!, only: :new
 
   def index
   end
@@ -24,7 +24,4 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
 
-  def redirect_signin
-    redirect_to new_user_session_path unless user_signed_in?
-  end
 end
