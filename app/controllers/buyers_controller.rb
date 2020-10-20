85527b9fe217fe2cbc842d07d43ac08f9df78dba
tomomomoaki <tomomomomorou333@gmail.com>
2020-10-20 12:13:02 +0900

    ビュー、コントローラー修正

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -2,7 +2,7 @@ class BuyersController < ApplicationController
 
   def index
     @buyer = Buyer.new
-    @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
   end
 
   def create
